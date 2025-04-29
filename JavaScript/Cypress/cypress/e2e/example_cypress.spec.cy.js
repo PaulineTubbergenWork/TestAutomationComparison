ecce02003dce86b7e86263a1f4298faaea74bb09
PaulineTubbergenWork <pauline.tubbergen@soprasteria.com>
2025-04-29 16:45:17 +0200

    changed cypress test to work with new version of wikipedia

--- JavaScript/Cypress/cypress/e2e/example_cypress.spec.cy.js
+++ JavaScript/Cypress/cypress/e2e/example_cypress.spec.cy.js
@@ -1,18 +1,37 @@
-import { basicUrl, checkZichtbaarheidEnInhoudVanVeld, englishUrl, titeltekst, typeInZoekveldEnKlikOpZoeken, welkomsttekst } from '../resources/wikipedia.po'
+import { basicUrl, checkZichtbaarheidEnInhoudVanVeld, englishUrl, klikKnopOmZoekveldTeOpenen, titeltekst, typeInZoekveldEnKlikOpZoeken, welkomsttekst } from '../resources/wikipedia.po'
 
 const zoekterm = 'Platypus'
 
 describe('Wikipedia', () => {
   it('checks that I can visit wikipedia', () => {
-      cy.visit(basicUrl)
+    cy.visit('https://www.wikipedia.org')
   })
 
-  it('Checks that I can find the Platypus page on the English Wikipedia', () => {
+  it('checks that I can find the Platypus page on the English Wikipedia', () => {
+    cy.log('go to the English Wikipedia page')
+    cy.visit('https://en.wikipedia.org/wiki/Main_Page')
+
+    cy.log('check that the page is in English')
+    cy.get('h1#Welcome_to_Wikipedia').should('be.visible')
+    cy.get('h1#Welcome_to_Wikipedia').should('contain.text', 'Welcome to')
+
+    cy.log('search for the Platypus')
+    cy.get('#p-search a').click()
+    cy.get('input[aria-label="Search Wikipedia"]').eq(0).type('Platypus')
+    cy.get('form#searchform button').click()
+
+    cy.log('check that the Platypus page was found')
+    cy.get('h1.firstHeading > span').should('be.visible')
+    cy.get('h1.firstHeading > span').should('contain.text', 'Platypus')
+  })
+
+  it('checks that I can find the Platypus page on the English Wikipedia, now with custom functions', () => {
     cy.log('go to the English Wikipedia page')
     cy.visit(englishUrl)
     cy.log('check that the page is in English')
     checkZichtbaarheidEnInhoudVanVeld(welkomsttekst, 'Welcome to')
     cy.log('search for the Platypus')
+    klikKnopOmZoekveldTeOpenen()
     typeInZoekveldEnKlikOpZoeken(zoekterm)
     cy.log('check that the Platypus page was found')
     checkZichtbaarheidEnInhoudVanVeld(titeltekst, zoekterm)
@@ -25,6 +44,7 @@ describe('Wikipedia', () => {
     // inhoud tekst is expres fout
     checkZichtbaarheidEnInhoudVanVeld(welkomsttekst, 'Welcom to')
     cy.log('search for the Platypus')
+    klikKnopOmZoekveldTeOpenen()
     typeInZoekveldEnKlikOpZoeken(zoekterm)
     cy.log('check that the Platypus page was found')
     checkZichtbaarheidEnInhoudVanVeld(titeltekst, zoekterm)
--- JavaScript/Cypress/cypress/resources/wikipedia.po.js
+++ JavaScript/Cypress/cypress/resources/wikipedia.po.js
@@ -1,6 +1,6 @@
 export const basicUrl = 'https://www.wikipedia.org'
 export const englishUrl = 'https://en.wikipedia.org/wiki/Main_Page'
-export const welkomsttekst = 'h1 > span.mw-headline'
+export const welkomsttekst = 'h1#Welcome_to_Wikipedia'
 export const titeltekst = 'h1.firstHeading > span'
 
 export const checkZichtbaarheidEnInhoudVanVeld = (cssSelector, tekst) => {
@@ -8,8 +8,12 @@ export const checkZichtbaarheidEnInhoudVanVeld = (cssSelector, tekst) => {
     cy.get(cssSelector).should('contain.text', tekst)
 }
 
+export const klikKnopOmZoekveldTeOpenen = () => {
+    cy.get('#p-search a').click()
+}
+
 export const typeInZoekveldEnKlikOpZoeken = (zoekterm) => {
-    const zoekveld = '#searchInput[aria-label="Search Wikipedia"]'
-    cy.get(zoekveld).type(zoekterm)
-    cy.contains('button', 'Search').click()
+    const zoekveld = 'input[aria-label="Search Wikipedia"]'
+    cy.get(zoekveld).eq(0).type(zoekterm)
+    cy.get('form#searchform button').click()
 }
