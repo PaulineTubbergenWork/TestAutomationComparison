161e5a85822872a02d868b54b42b5e8653628cfb
PaulineTubbergenWork <pauline.tubbergen@soprasteria.com>
2025-08-05 19:05:54 +0200

    removed tests for extra websites

--- JavaScript/Cypress/cypress/e2e/petstoreTests.spec.cy.js
+++
@@ -1,9 +0,0 @@
-describe('Petstore tests', () => {
-    it('checks that I can visit the petstore', () => {
-        cy.visit('https://petstore.octoperf.com/');
-        cy.contains('a', 'Enter the Store').click();
-        cy.get('#MainImage').should('be.visible');
-        cy.get('#MainImage area[alt="Reptiles"]').click();
-        cy.get('#Catalog h2').should('have.text', 'Reptiles').and('be.visible');
-    });
-});
