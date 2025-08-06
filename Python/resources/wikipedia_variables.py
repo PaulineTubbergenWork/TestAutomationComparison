f3f7eab0b5e46706e7e7883ecf557f577d9534de
PaulineTubbergenWork <pauline.tubbergen@soprasteria.com>
2025-08-06 23:43:26 +0200

    repaired python variables

--- Python/resources/wikipedia_variables.py
+++ Python/resources/wikipedia_variables.py
@@ -1,6 +1,6 @@
 # variables
 english_url = "https://en.wikipedia.org/wiki/Main_Page"
-search_field = "#input[aria-label='Search Wikipedia']"
+search_field = "input[aria-label='Search Wikipedia']"
 search_button = "form#searchform button"
-welcome_text = "h1 > span.mw-headline"
+welcome_text = "h1#Welcome_to_Wikipedia"
 title_text = "h1.firstHeading > span"
