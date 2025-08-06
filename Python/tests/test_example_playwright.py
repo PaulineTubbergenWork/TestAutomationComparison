5e26cc070b9409c53fd1b45ec7a66f05febcd75e
PaulineTubbergenWork <pauline.tubbergen@soprasteria.com>
2025-08-06 21:53:57 +0200

    added python tests without custom functions or variables

--- Python/tests/test_example_playwright.py
+++ Python/tests/test_example_playwright.py
@@ -1,19 +1,33 @@
-from playwright.sync_api import Page
+from playwright.sync_api import Page, expect
 from resources.wikipedia_playwright import open_url_in_browser, check_field_visibility_and_content, \
     type_text_in_search_field_and_click_search_button
 from resources.wikipedia_variables import english_url, welcome_text, title_text
 
-zoekterm = "Platypus"
+
+search_term = "Platypus"
+
 
 def test_pw_checks_that_i_can_find_the_platypus_page_on_the_english_wikipedia(page: Page):
+    print("go to the English Wikipedia page")
+    page.goto("https://en.wikipedia.org/wiki/Main_Page")
+    print("checks that the page is in English")
+    expect(page.locator("css=h1 > span.mw-headline")).to_contain_text("Welcome to")
+    print("search for the Platypus")
+    page.locator("css=#input[aria-label='Search Wikipedia']").fill("Platypus")
+    page.locator("css=form#searchform button").click()
+    print("check that the Platypus page was found")
+    expect(page.locator("css=h1.firstHeading > span")).to_contain_text("Platypus")
+
+
+def test_pw_checks_that_i_can_find_the_platypus_page_on_the_english_wikipedia_with_custom_functions(page: Page):
     print("go to the English Wikipedia page")
     open_url_in_browser(page, english_url)
     print("checks that the page is in English")
     check_field_visibility_and_content(page, welcome_text, "Welcome to")
     print("search for the Platypus")
-    type_text_in_search_field_and_click_search_button(page, zoekterm)
+    type_text_in_search_field_and_click_search_button(page, search_term)
     print("check that the Platypus page was found")
-    check_field_visibility_and_content(page, title_text, zoekterm)
+    check_field_visibility_and_content(page, title_text, search_term)
 
 
 def test_pw_fails_on_purpose(page: Page):
@@ -23,6 +37,6 @@ def test_pw_fails_on_purpose(page: Page):
     # text content is wrong on purpose
     check_field_visibility_and_content(page, welcome_text, "Welcom to")
     print("search for the Platypus")
-    type_text_in_search_field_and_click_search_button(page, zoekterm)
+    type_text_in_search_field_and_click_search_button(page, search_term)
     print("check that the Platypus page was found")
-    check_field_visibility_and_content(page, titeltekst, zoekterm)
+    check_field_visibility_and_content(page, titeltekst, search_term)
--- Python/tests/test_example_selenium.py
+++ Python/tests/test_example_selenium.py
@@ -1,21 +1,40 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from resources.wikipedia_variables import english_url, welcome_text, title_text
 from resources.wikipedia_selenium import open_url_in_browser, check_field_visibility_and_content, \
     type_text_in_search_field_and_click_search_button
 
-zoekterm = "Platypus"
+search_term = "Platypus"
 
 
 def test_checks_that_i_can_find_the_platypus_page_on_the_english_wikipedia():
+    browser_chrome = webdriver.Chrome()
+    print("go to the English Wikipedia page")
+    browser_chrome.get("https://en.wikipedia.org/wiki/Main_Page")
+    print("checks that the page is in English")
+    selector = browser_chrome.find_element(By.CSS_SELECTOR, "h1 > span.mw-headline")
+    assert selector.is_displayed() == True
+    assert "Welcome to" in selector.text
+    print("search for the Platypus")
+    assert browser_chrome.find_element(By.CSS_SELECTOR, "#input[aria-label='Search Wikipedia']").is_displayed()
+    browser_chrome.find_element(By.CSS_SELECTOR, "#input[aria-label='Search Wikipedia']").send_keys("Platypus")
+    browser_chrome.find_element(By.CSS_SELECTOR, "form#searchform button").click()
+    print("check that the Platypus page was found")
+    assert browser_chrome.find_element(By.CSS_SELECTOR, "h1.firstHeading > span").is_displayed() == True
+    assert "Platypus" in browser_chrome.find_element(By.CSS_SELECTOR, "h1.firstHeading > span").text
+    browser_chrome.close()
+
+
+def test_checks_that_i_can_find_the_platypus_page_on_the_english_wikipedia_with_custom_functions():
     browser_chrome = webdriver.Chrome()
     print("go to the English Wikipedia page")
     open_url_in_browser(browser_chrome, english_url)
     print("checks that the page is in English")
     check_field_visibility_and_content(browser_chrome, welcome_text, "Welcome to")
     print("search for the Platypus")
-    type_text_in_search_field_and_click_search_button(browser_chrome, zoekterm)
+    type_text_in_search_field_and_click_search_button(browser_chrome, search_term)
     print("check that the Platypus page was found")
-    check_field_visibility_and_content(browser_chrome, title_text, zoekterm)
+    check_field_visibility_and_content(browser_chrome, title_text, search_term)
     browser_chrome.close()
 
 
@@ -27,7 +46,7 @@ def test_fails_on_purpose():
     # text content is wrong on purpose
     check_field_visibility_and_content(browser_chrome, welcome_text, "Welcom to")
     print("search for the Platypus")
-    type_text_in_search_field_and_click_search_button(browser_chrome, zoekterm)
+    type_text_in_search_field_and_click_search_button(browser_chrome, search_term)
     print("check that the Platypus page was found")
-    check_field_visibility_and_content(browser_chrome, title_text, zoekterm)
+    check_field_visibility_and_content(browser_chrome, title_text, search_term)
     browser_chrome.close()
